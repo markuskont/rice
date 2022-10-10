b929420a1ccf3ae01fbb6331be9d426e0356a8de
Markus Kont <markus@stamus-networks.com>
2022-10-10 10:51:01 +0300

    lvim: implement Go IDE
    ref: https://github.com/LunarVim/starter.lvim/blob/go-ide/config.lua

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -163,12 +163,14 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-  { "folke/tokyonight.nvim" },
+  {"folke/tokyonight.nvim"},
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
-  { "morhetz/gruvbox" }
+  {"morhetz/gruvbox"},
+  {"olexsmir/gopher.nvim"},
+  {"leoluz/nvim-dap-go"},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -185,10 +187,12 @@ lvim.plugins = {
 --   end,
 -- })
 
--- Python IDE
+-- IDE setups
 local formatters = require "lvim.lsp.null-ls.formatters"
+
+-- Python IDE
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
+  { command = "autopep8", filetypes = { "python" } },
 }
 
 -- Set a linter.
@@ -211,3 +215,99 @@ lvim.builtin.which_key.vmappings["d"] = {
   name = "Debug",
   s = { "<cmd>lua require('dap-python').debug_selection()<cr>", "Debug Selection" },
 }
+
+-- Go IDE
+------------------------
+-- Treesitter
+------------------------
+lvim.builtin.treesitter.ensure_installed = {
+  "go",
+  "gomod",
+}
+
+------------------------
+-- Formatting
+------------------------
+formatters.setup {
+  { command = "goimports", filetypes = { "go" } },
+  { command = "gofumpt", filetypes = { "go" } },
+}
+
+lvim.format_on_save = {
+  pattern = { "*.go" },
+}
+
+
+------------------------
+-- LSP
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+local status_ok, gopher = pcall(require, "gopher")
+if not status_ok then
+  return
+end
+
+gopher.setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
+
+------------------------
+-- Language Key Mappings
+------------------------
+lvim.builtin.which_key.mappings["L"] = {
+  name = "Go",
+  i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
+  t = { "<cmd>GoMod tidy<cr>", "Tidy" },
+  a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
+  A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
+  e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
+  g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
+  f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
+  c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
+}
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+lvim.builtin.which_key.mappings["dT"] = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" }
