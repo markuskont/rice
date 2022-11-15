60bea4d923491df22ab0f56a1b6a8146a0a4962d
Markus Kont <alias013@gmail.com>
2022-11-15 14:42:12 +0200

    lvim: relative line numbers

--- .config/lvim/lua/user/core.lua
+++ .config/lvim/lua/user/core.lua
@@ -36,3 +36,6 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
+
+-- core vim options
+vim.opt.relativenumber = true
